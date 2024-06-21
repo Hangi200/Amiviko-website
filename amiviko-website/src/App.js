2ffb640b4495d1db44c1bb372a1cac268e02035f
stella <stellahjames200@gmail.com>
2024-06-21 16:05:00 +0300

    Initial commit

--- amiviko-website/src/App.js
+++ amiviko-website/src/App.js
@@ -1,18 +1,18 @@
+// src/App.js
 
-import './App.css';
-import Navbar from './components/Navbar';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      {/* <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />{" "} */}
+      <Navbar />
+      <header className="App-header">
+        <h1>Your Events With US</h1>
+      </header>
+    </div>
   );
 }
 
--- amiviko-website/src/components/Navbar.js
+++ amiviko-website/src/components/Navbar.js
@@ -1,19 +1,72 @@
-import React, { useState } from 'react';
-import { link } from 'react-router-dom';
+// src/components/Navbar.js
 
-function Navbar() {
+// import React from "react";
+// import "./Navbar.css"; // We'll create this CSS file for styling
+
+
+// const Navbar = () => {
+//   return (
+//     <nav className="navbar">
+//       <div className="navbar-logo">
+//         <a href="/">
+//           <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />{" "}
+//           Amiviko
+//         </a>
+//       </div>
+//       <ul className="navbar-links">
+//         <li>
+//           <a href="/">Home</a>
+//         </li>
+//         <li>
+//           <a href="/about">About</a>
+//         </li>
+//         <li>
+//           <a href="/Booking">Booking</a>
+//         </li>
+//         <li>
+//           <a href="/contact">Contact</a>
+//         </li>
+//         <li>
+//           <a href="/Login">
+//             <button className="navbar-button">Login</button>
+//           </a>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default Navbar;
+import React from "react";
+ // Adjust the path based on your directory structure
+import "./Navbar.css";
+
+const Navbar = () => {
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          {/* <link to='/' className='navbar-logo'> */}
-          AMIVIKO{" "}
-          <img src={`${process.env.PUBLIC_URL}/images/amiviko-website/public/images/amiviko_logo-removebg-preview.png`} alt="Logo" />
-          {/* </link> */}
-        </div>
-      </nav>
-    </>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="/">
+          <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />{" "}
+        </a>
+        <h2>Amiviko</h2>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+      <button className="navbar-button">Login</button>
+    </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
