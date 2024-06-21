8f7804fbdfdd4d0dc4988f9ef9d6ecc6785ce832
stella <stellahjames200@gmail.com>
2024-06-21 16:59:42 +0300

    Initial commit

--- amiviko-website/src/App.js
+++ amiviko-website/src/App.js
@@ -2,13 +2,14 @@
 
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      {/* <img src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Logo" />{" "} */}
       <Navbar />
+      <Home />
       <header className="App-header">
         <h1>Your Events With US</h1>
       </header>
