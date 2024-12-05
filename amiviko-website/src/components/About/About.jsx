331c1ce081454c3e208380b5943bf8d72aa9a70e
stella <stellahjames200@gmail.com>
2024-12-05 15:36:06 +0300

    Initial commit

--- amiviko-website/src/components/About/About.jsx
+++ amiviko-website/src/components/About/About.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import './about.scss'
-
+import img6 from "../../Assets/pic8.png";
 
 function About() {
   return (
     <section className="about">
-      <container className="container">
-        <div className="about_section">
+      <div className="about_section">
+        <div className="section_one flex">
           <h4 className="section_title"> About Us</h4>
+          <div className="footerParagraph ">
+            Welcome to Amiviko, your premier destination for unforgettable
+            events and experiences. We are passionate about creating moments
+            that matter, blending creativity, meticulous planning, and
+            exceptional service to bring your vision to life.
+          </div>
+          <div>
+            <p>
+              Amiviko empowers event planners with tools to create, manage, and
+              promote events effortlessly.
+            </p>
+            <p>
+              Discover events tailored to your interests. From concerts and
+              conferences to community meetups.
+            </p>
+            <p>
+              Showcase your services and spaces to a growing audience of
+              organizers and participants, making it easier than ever to connect
+              and collaborate.
+            </p>
+            <p>
+              Amplify your brand by sponsoring events that align with your
+              vision. Amiviko provides a platform to reach targeted audiences
+              and measure your impact.
+            </p>
+          </div>
         </div>
-
-      </container>
+        <div className="section_two">
+          <img src={img6} alt="" className="w-100" />
+        </div>
+      </div>
     </section>
   );
 }
